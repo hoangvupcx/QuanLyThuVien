4cdeda9bab6397316091ccf2d9482e4d1b15ccab
hoangvupcx <89677053+hoangvupcx@users.noreply.github.com>
2023-03-28 14:12:39 +0700

    Update

--- QuanLyThuVien/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.nguyenhoangvu.quanlythuvien {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    
-    opens com.ktpm.quanlythuvien to javafx.fxml;
-    exports com.ktpm.quanlythuvien;
-}
